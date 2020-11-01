644dde5281dae18d4783c236b91a4f29f58a5358
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-11-01 16:18:10 +0300

    Improve SQL script

--- DockerEnvironment/sqldump/install.sql
+++ DockerEnvironment/sqldump/install.sql
@@ -8,10 +8,10 @@ CREATE TABLE `token` (
   `username` varchar(255) NOT NULL,
   `command` varchar(255) NOT NULL,
   `permission` varchar(255) NOT NULL,
-  `creation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `creation` varchar(255) NOT NULL,
   `expire` varchar(255) NOT NULL,
   `creator` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `token`
   ADD UNIQUE KEY `token` (`token`);
